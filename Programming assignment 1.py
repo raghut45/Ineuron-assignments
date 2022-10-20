f926ce5b295faaec5198ce28cacdd52c34aec43c
raghut45 <54216054+raghut45@users.noreply.github.com>
2022-10-20 12:11:24 +0530

    Add files via upload
    Prgramming assignmnet 1`

---
+++ Programming assignment 1.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Write a Python program to print "Hello Python"?
+# 
+
+# In[2]:
+
+
+print("Hello Python")
+
+
+# 2.Write a Python program to do arithmetical operations addition and division?
+
+# In[17]:
+
+
+a=5
+b=4
+
+
+# In[18]:
+
+
+#addition
+x=a+b
+x
+
+
+# In[19]:
+
+
+#division
+y=a/b
+y
+
+
+# 3. Write a Python program to find the area of a triangle?
+
+# In[20]:
+
+
+#Area of triangle
+b=int(input("enter the base:"))
+h=int(input("enter the height:"))
+Area=0.5*b*h
+
+
+# In[21]:
+
+
+Area
+
+
+# 4.Write a Python program to swap two variables?
+
+# In[22]:
+
+
+w=23
+d=45
+w,d=d,w
+print("Value of w:", w)
+print("Value of d:", d)
+
+
+# 5. Write a Python program to generate a random number?
+
+# In[24]:
+
+
+import random
+r=random.random()
+r
+
+
+# In[ ]:
+
+
+
+
